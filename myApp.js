b17b67c6b535f30dc0bf241fd5ead95caf64c58d
Vajira <vajirawke@gmail.com>
2022-10-03 11:42:20 +0530

    ifindandRemove

--- myApp.js
+++ myApp.js
@@ -87,7 +87,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({_id:personId},(err,data)=>{
+    if(err) console.log(err);
+    done(null , data);
+  })  
 };
 
 const removeManyPeople = (done) => {
